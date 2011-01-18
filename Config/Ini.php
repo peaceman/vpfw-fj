03d6b2857fa5039e083832c0fca9fd3e954d7fde
tobiasdev <tobiasdev@live.de>
2011-01-19 00:31:22 +0100

    fixed typo

--- Config/Ini.php
+++ Config/Ini.php
@@ -1,14 +1,14 @@
 <?php
 
 class Vpfw_Config_Ini extends Vpfw_Config_Abstract {
-    protected function  parseFile() {
+    protected function parseFile() {
         parent::parseFile();
         // Wir benutzen eine PHP- Funktion um die Ini- Datei in den
         // Config- Array zu prügeln
         $this->configArray = parse_ini_file($this->fileName, true);
     }
 
-    protected function  writeBack() {
+    protected function writeBack() {
         $fileHandle = parent::writeBack();
         $iniStr = '';
         
@@ -16,21 +16,20 @@ class Vpfw_Config_Ini extends Vpfw_Config_Abstract {
         foreach ($this->configArray as $sectionName => $values) {
             // Der Konfigurationsarray muss Sections (arrays enhalten) sonst ist er invalid!
             if (!is_array($values))
-                throw new Vpfw_Exception_Logical ('Config array have to contain sections!');
+                throw new Vpfw_Exception_Logical('Config array have to contain sections!');
 
-            $iniStr.='['.$sectionName.']'.PHP_EOL;
+            $iniStr .= '['.$sectionName.']' . PHP_EOL;
 
             // Wir gehen nun alle Werte der Section durch und
             // schreiben sie in die Konfigurationsdatei
             foreach ($values as $keyName => $value) {
-                if (!is_null($value))
-                {
+                if (!is_null($value)) {
                     if (is_bool($value) || is_int($value))
-                        $iniStr.=$keyName.' = '.$value.PHP_EOL;
+                        $iniStr .= $keyName . ' = ' . $value . PHP_EOL;
                     else if(is_string($value))
-                        $iniStr.=$keyName.' = "'.$value.'"'.PHP_EOL;
+                        $iniStr .= $keyName . ' = "' . $value . '"' . PHP_EOL;
                     else
-                        throw new Vpfw_Exception_InvalidArgument ('Configuration type does not support datatype!');
+                        throw new Vpfw_Exception_InvalidArgument('Configuration type does not support datatype!');
                 }
             }
         }
