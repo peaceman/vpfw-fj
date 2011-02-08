975ee6bced679cc2d29c24087904cd7b1d1f6c1c
peaceman <nico.naegele@telekom.de>
2011-02-08 13:07:02 +0100

    if the dbsesid from session doesnt exist in database, no error occurs anymore

--- Vpfw/Auth/Session.php
+++ Vpfw/Auth/Session.php
@@ -1,13 +1,11 @@
 <?php
 class Vpfw_Auth_Session {
     /**
-     *
      * @var Vpfw_Auth_Storage_Interface
      */
     private $storage;
 
     /**
-     *
      * @var Vpfw_Auth_Adapter_Interface
      */
     private $adapter;
@@ -33,10 +31,10 @@ class Vpfw_Auth_Session {
     private $rbacUser;
 
     /**
-     *
      * @param Vpfw_Auth_Adapter_Interface $adapter
      * @param Vpfw_Auth_Storage_Interface $storage
      * @param App_DataMapper_Session $sessionMapper
+     * @param Vpfw_Request_Interface $request
      */
     public function __construct(Vpfw_Auth_Adapter_Interface $adapter, Vpfw_Auth_Storage_Interface $storage, App_DataMapper_Session $sessionMapper, Vpfw_Request_Interface $request) {
         $this->adapter = $adapter;
@@ -44,6 +42,61 @@ class Vpfw_Auth_Session {
         $this->sessionMapper = $sessionMapper;
         $this->request = $request;
         $this->reattachOldOrCreateNewSession();
+    }
+
+    private function reattachOldOrCreateNewSession() {
+        if (true == is_null($this->storage->get('DbSesId'))) {
+            $this->createNewSession();
+        } else {
+            $this->tryToReattachOldSession();
+        }
+    }
+
+    private function createNewSession() {
+        $sessionData = array(
+            'Ip' => $this->request->getRemoteAddress(),
+            'StartTime' => time(),
+            'LastRequest' => time(),
+            'Hits' => 1,
+            'UserAgent' => $this->request->getHeader('user-agent')
+        );
+
+        $this->session = $this->sessionMapper->createEntry($sessionData, true);
+        $this->storage->set('DbSesId', $this->session->getId());
+    }
+
+    private function tryToReattachOldSession() {
+        if (false == $this->tryToGetSessionFromDatabase()) {
+            $this->createNewSession();
+        } elseif (true == $this->isOldSessionHijacked()) {
+            $this->createNewSession();
+        } else {
+            $this->reattachOldSession();
+        }
+    }
+
+    private function tryToGetSessionFromDatabase() {
+        try {
+            $this->session = $this->sessionMapper->getEntryById($this->storage->get('DbSesId'));
+        } catch (Vpfw_Exception_OutOfRange $e) {
+            return false;
+        }
+        return true;
+    }
+
+    private function isOldSessionHijacked() {
+        if ($this->session->getIp() != $this->request->getRemoteAddress()) {
+            return true;
+        }
+        if ($this->session->getUserAgent() != $this->request->getHeader('user-agent')) {
+            return true;
+        }
+        return false;
+    }
+
+    private function reattachOldSession() {
+        $this->session->setLastRequest(time());
+        $this->session->setHits($this->session->getHits() + 1);
     }
 
     /**
@@ -69,51 +122,6 @@ class Vpfw_Auth_Session {
      */
     public function isLoggedIn() {
         if (false == is_null($this->session->getUser())) {
-            return true;
-        }
-        return false;
-    }
-
-    private function reattachOldOrCreateNewSession() {
-        if (true == is_null($this->storage->get('DbSesId'))) {
-            $this->createNewSession();
-        } else {
-            $this->tryToReattachOldSession();
-        }
-    }
-
-    private function createNewSession() {
-        $sessionData = array(
-            'Ip' => $this->request->getRemoteAddress(),
-            'StartTime' => time(),
-            'LastRequest' => time(),
-            'Hits' => 1,
-            'UserAgent' => $this->request->getHeader('user-agent')
-        );
-        
-        $this->session = $this->sessionMapper->createEntry($sessionData, true);
-        $this->storage->set('DbSesId', $this->session->getId());
-    }
-
-    private function reattachOldSession() {
-        $this->session->setLastRequest(time());
-        $this->session->setHits($this->session->getHits() + 1);
-    }
-
-    private function tryToReattachOldSession() {
-        $this->session = $this->sessionMapper->getEntryById($this->storage->get('DbSesId'));
-        if (true == $this->isOldSessionHijacked()) {
-            $this->createNewSession();
-        } else {
-            $this->reattachOldSession();
-        }
-    }
-
-    private function isOldSessionHijacked() {
-        if ($this->session->getIp() != $this->request->getRemoteAddress()) {
-            return true;
-        }
-        if ($this->session->getUserAgent() != $this->request->getHeader('user-agent')) {
             return true;
         }
         return false;
